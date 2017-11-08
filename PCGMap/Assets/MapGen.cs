e72535b43db1914aba6fd57717549ba53911afc9
Rayner <robrayner96@hotmail.com>
2017-11-08 23:36:36 +0000

    Init

---
+++ PCGMap/Assets/MapGen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MapGen : MonoBehaviour {
+
+	public Vector3 TerrainSize;
+
+	// Use this for initialization
+	void Start () {
+		TerrainData tData = new TerrainData ();
+		tData.size = TerrainSize;
+		Terrain.CreateTerrainGameObject (tData);
+		GameObject.FindObjectOfType<Terrain> ().gameObject.AddComponent<TerDat> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}		
+}
---
+++ PCGMap/Assets/TerDat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerDat : MonoBehaviour {
+
+	TerrainData TerData;
+
+	// Use this for initialization
+	void Awake () {
+		TerData = gameObject.GetComponent<TerrainData> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
